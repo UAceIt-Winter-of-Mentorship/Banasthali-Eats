a892b881e53db05e27f892c6a9f8cd81c11c56b5
AyushAgr145 <ayushagr2001@gmail.com>
2022-01-25 20:16:00 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/banasthali_eats/Profile.java
@@ -0,0 +1,22 @@
+package com.example.banasthali_eats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Profile extends AppCompatActivity {
+    Button back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        back = findViewById(R.id.back);
+    }
+    public void fun(View view)
+    {
+        startActivity(new Intent(Profile.this, Login.class));
+    }
+}
