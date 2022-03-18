aff558c4a7fa75eeedf71005e88d37e2a8a77f4a
Aditi Tripathi <atan372002@gmail.com>
2022-03-18 17:38:29 +0530

    First Commit

---
+++ app/src/main/java/com/example/banasthali_eats/order_placed.java
@@ -0,0 +1,14 @@
+package com.example.banasthali_eats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class order_placed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_placed);
+    }
+}
